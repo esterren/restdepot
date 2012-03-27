d4c79818b05e5982f1909c8458dde1090a1e7054
esterren <renato.estermann@gmail.com>
2012-03-27 12:59:43 +0200

    validation and unit tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, :presence => true
 	validates :title, :uniqueness =>true
-	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.05}
+	validates :price, :format => {
+		:with		=> %r{^\d*\.\d[5]?$}
+	}
 	validates :image_url, :format => {
 		:with		=> %r{\.(gif|jpg|png)$}i,
 		:message	=> 'must be a URL for GIF, JPG or PNG image.'
