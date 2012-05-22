4d05e53c11e51dcfd2d1126d5ca6babb4f6f8da4
esterren <renato.estermann@gmail.com>
2012-05-22 11:33:43 +0200

    Cap 10, Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,17 +40,18 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-	@cart = current_cart
-	product = Product.find(params[:product_id])
-	@line_item = @cart.line_items.build(product: product)
-    
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-		format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.html { redirect_to @line_item.cart }
+        format.json { render json: @line_item,
+          status: :created, location: @line_item }
       else
         format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
